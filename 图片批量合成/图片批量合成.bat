15cf6df8be8a7d41527cfd9f8a903866ca80bc12
savet-save <savet_save@163.com>
2023-06-25 15:31:10 +0800

    optimize: 优化图片批量合成脚本的格式

--- 图片批量合成/图片批量合成.bat
+++ 图片批量合成/图片批量合成.bat
@@ -7,15 +7,15 @@ set output_folder=output
 @rem 检测运行环境
 where magick >nul 2>nul
 if %errorlevel% neq 0 (
-  echo The running environment is not supported 
-  echo plz to 'https://imagemagick.org/script/download.php#windows' download and install
-  pause
-  exit
+    echo The running environment is not supported 
+    echo plz to 'https://imagemagick.org/script/download.php#windows' download and install
+    pause
+    exit
 )
 
 @rem 创建输出文件夹
 if not exist !output_folder! (
-   md !output_folder!
+    md !output_folder!
 )
 
 @rem 获取当前目录下所有的文件夹，但排除输出文件夹
@@ -29,9 +29,9 @@ for /d %%a in (*) do (
 
 @rem 检查是否存在第二个文件夹
 if not exist !input_dirs[2]! (
-   echo not has second folder
-   pause
-   exit
+    echo not has second folder
+    pause
+    exit
 )
 
 @rem 获取图片
@@ -46,17 +46,17 @@ set i=0
 set /a max_mixed_num=0
 for %%j in (!input_dirs[2]!\*.png !input_dirs[2]!\*.jpg) do (
     set /a i+=1
-	set /a max_mixed_num+=1
+    set /a max_mixed_num+=1
     set "img_2[!i!]=%%j"
 )
 
 @rem 遍历数组并输出每一个元素
 for /l %%i in (1, 1, %max_mixed_num%) do (
-	if exist "!img_1[%%i]!" (
-		if exist "!img_2[%%i]!" (
-			echo form !img_1[%%i]! !img_2[%%i]! to !output_folder!\%%i.png
-			magick montage "!img_1[%%i]!" "!img_2[%%i]!" -tile 2x1 -geometry +0+0  "!output_folder!\%%i.png"
-		)
+    if exist "!img_1[%%i]!" (
+        if exist "!img_2[%%i]!" (
+            echo form !img_1[%%i]! !img_2[%%i]! to !output_folder!\%%i.png
+            magick montage "!img_1[%%i]!" "!img_2[%%i]!" -tile 2x1 -geometry +0+0  "!output_folder!\%%i.png"
+        )
     )
 )
 
